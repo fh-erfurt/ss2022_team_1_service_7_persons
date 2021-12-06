3ae95b5a1a0a8cd2cfd29f503143c262ecbe1959
jo5642li <jonas.liehmann@fh-erfurt.de>
2021-12-06 21:19:49 +0100

    added initial classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
 
-
 public class Main {
+    private static Position[] myEnum = { Position.Dozent, Position.Dekan };
+
     public static void main(String[] args) {
+        for (Position currentEnum : myEnum) {
+            System.out.println(currentEnum);
+        }
+
         System.out.println("Hello World");
     }
 }
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,13 @@
 public class Person {
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    private String phone;
+
 }
