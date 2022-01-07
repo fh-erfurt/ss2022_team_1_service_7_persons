81cd191bb13622b6d7b180e7eec056c173f8c13d
niklas <niklas.schumann@fh-erfurt.de>
2022-01-07 18:12:29 +0100

    auch test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,13 @@
 
 public class Main {
-    /*private static Position[] myEnum = { Position.Dozent, Position.Dekan };
+  /*  private static Position[] myEnum = { Position.Dozent, Position.Dekan };
 
     public static void main(String[] args) {
         for (Position currentEnum : myEnum) {
             System.out.println(currentEnum);
         }
-
+        */
+        public static void main(String[] args) {
         System.out.println("Hello World");
-    }*/
+    }
 }
