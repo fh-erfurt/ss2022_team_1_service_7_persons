511080cc02d323d9cafe4f815538a308526fc518
DevTobias <tobi.kaerst@gmx.de>
2022-03-23 21:02:05 +0100

    added missing doc comments

--- person-service/src/main/java/de/fherfurt/person/person/business/PersonBF.java
+++ person-service/src/main/java/de/fherfurt/person/person/business/PersonBF.java
@@ -1,5 +1,7 @@
 package de.fherfurt.person.person.business;
 
+import de.fherfurt.person.core.persistence.errors.NoResultException;
+import de.fherfurt.person.core.persistence.errors.ToManyResultsException;
 import de.fherfurt.person.person.entity.Person;
 import de.fherfurt.person.person.entity.PersonRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,26 +21,61 @@ public class PersonBF {
 
     private final PersonRepository personRepository = PersonRepository.of();
 
+    /**
+     * Save a person to the underlying storage.
+     *
+     * @param person Instance to save
+     */
     public void save(final Person person) {
         personRepository.save(person);
     }
 
+    /**
+     * Find an person by its id. If no entity is available, an empty
+     * {@link Optional} is returned.
+     *
+     * @param id Id of the searched entity
+     * @return The person or empty
+     */
     public Optional<Person> findBy(final int id) {
         return personRepository.findBy(id);
     }
 
+    /**
+     * Find a person by its email. If no person is available, an empty {@link Optional} is returned.
+     *
+     * @param email Email of the searched entity
+     * @return The person or empty
+     */
     public Optional<Person> findByEmail(String email) {
         return personRepository.findByEmail(email);
     }
 
+    /**
+     * Find persons by its name. If no person is available, an empty {@link List} is returned.
+     *
+     * @param name The name (phrase) of the person.
+     * @return The persons or empty list
+     */
     public List<Person> findByName(String name) {
         return personRepository.findByName(name);
     }
 
+    /**
+     * Find persons by its faculty id. If no person is available, an empty {@link List} is returned.
+     *
+     * @param facultyId The faculty id of the persons
+     * @return The persons or empty list
+     */
     public List<Person> findByFaculty(int facultyId) {
         return personRepository.findByFaculty(facultyId);
     }
 
+    /**
+     * Deletes a person by its id.
+     *
+     * @param id Id of the person to delete.
+     */
     public void delete(final int id) {
         final Optional<Person> toDelete = findBy(id);
 
